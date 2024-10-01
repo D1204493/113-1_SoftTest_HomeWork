a22f469061a0a8ea65f467c24c5b964a447f04a5
D1204493 <161418128+D1204493@users.noreply.github.com>
2024-10-01 20:14:17 +0800

    HW1
    MLB外卡資格排序，2023年測試資料，還未做測試(ex:assert)

---
+++ HW1_MLBSort_0926/src/main/java/org/example/Main.java
@@ -0,0 +1,129 @@
+package org.example;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+  public static void main(String[] args) {
+    String al_one, al_two, al_three, al_four, al_five, al_six; //美聯前6名
+    String nl_one, nl_two, nl_three, nl_four, nl_five, nl_six; //國聯前6名
+
+    MlbSort ms = new MlbSort();
+    ms.load("2023MLB_regularSeason.csv");
+    ms.printTeamsByArea(); //檢查用
+
+    //美聯各區第一名
+    Team alEastFirst = ms.alEastTeams.get(0); // 取得東區第一個 Team
+    Team alCentralFirst = ms.alCentralTeams.get(0); // 取得中區第一個 Team
+    Team alWestFirst = ms.alWestTeams.get(0); // 取得西區第一個 Team
+    //國聯各區第一名
+    Team nlEastFirst = ms.nlEastTeams.get(0); // 取得東區第一個 Team
+    Team nlCentralFirst = ms.nlCentralTeams.get(0); // 取得中區第一個 Team
+    Team nlWestFirst = ms.nlWestTeams.get(0); // 取得西區第一個 Team
+
+    // 建立一個 ArrayList 並將美聯各區第一名球隊加入
+    ArrayList<Team> al_firstPlaceTeams = new ArrayList<>();
+    al_firstPlaceTeams.add(alEastFirst);
+    al_firstPlaceTeams.add(alCentralFirst);
+    al_firstPlaceTeams.add(alWestFirst);
+
+    // 國聯各區第一名球隊加入 ArrayList
+    ArrayList<Team> nl_firstPlaceTeams = new ArrayList<>();
+    nl_firstPlaceTeams.add(nlEastFirst);
+    nl_firstPlaceTeams.add(nlCentralFirst);
+    nl_firstPlaceTeams.add(nlWestFirst);
+
+    // 調用排序方法對美聯與國聯各區第一名之球隊進行排序
+    al_firstPlaceTeams = sortTeamsByPct(al_firstPlaceTeams);
+    nl_firstPlaceTeams = sortTeamsByPct(nl_firstPlaceTeams);
+
+    // 將美聯各區第一名排序結果放入變數
+    al_one = al_firstPlaceTeams.get(0).getTeamName();
+    al_two = al_firstPlaceTeams.get(1).getTeamName();
+    al_three = al_firstPlaceTeams.get(2).getTeamName();
+    // 將國聯各區第一名排序結果放入變數
+    nl_one = nl_firstPlaceTeams.get(0).getTeamName();
+    nl_two = nl_firstPlaceTeams.get(1).getTeamName();
+    nl_three = nl_firstPlaceTeams.get(2).getTeamName();
+
+
+    //美聯"非"第一名之球隊 ArrayList
+    ArrayList<Team> al_notFirstPlaceTeams = new ArrayList<>();
+    //國聯"非"第一名之球隊 ArrayList
+    ArrayList<Team> nl_notFirstPlaceTeams = new ArrayList<>();
+    for(int i=1; i<5; i++){
+      //美聯各區"非"第一名球隊加入 ArrayList
+      al_notFirstPlaceTeams.add(ms.alEastTeams.get(i));
+      al_notFirstPlaceTeams.add(ms.alCentralTeams.get(i));
+      al_notFirstPlaceTeams.add(ms.alWestTeams.get(i));
+
+      //國聯各區"非"第一名球隊加入 ArrayList
+      nl_notFirstPlaceTeams.add(ms.nlEastTeams.get(i));
+      nl_notFirstPlaceTeams.add(ms.nlCentralTeams.get(i));
+      nl_notFirstPlaceTeams.add(ms.nlWestTeams.get(i));
+    }
+    System.out.println("AL_notFirstPlaceTeams:"+al_notFirstPlaceTeams);
+    System.out.println("NL_notFirstPlaceTeams:"+nl_notFirstPlaceTeams);
+    // 美聯"非"第一名之排序
+    al_notFirstPlaceTeams = sortTeamsByPct(al_notFirstPlaceTeams);
+    System.out.println("排序AL_notFirstPlaceTeams:"+al_notFirstPlaceTeams);
+    // 抓取美聯"非"第一名隊伍排序後的前三名，並放入變數
+    al_four = al_notFirstPlaceTeams.get(0).getTeamName();
+    al_five = al_notFirstPlaceTeams.get(1).getTeamName();
+    al_six = al_notFirstPlaceTeams.get(2).getTeamName();
+
+    // 國聯"非"第一名之排序
+    nl_notFirstPlaceTeams = sortTeamsByPct(nl_notFirstPlaceTeams);
+    System.out.println("排序NL_notFirstPlaceTeams:"+nl_notFirstPlaceTeams);
+    // 抓取國聯"非"第一名隊伍排序後的前三名，並放入變數
+    nl_four = nl_notFirstPlaceTeams.get(0).getTeamName();
+    nl_five = nl_notFirstPlaceTeams.get(1).getTeamName();
+    nl_six = nl_notFirstPlaceTeams.get(2).getTeamName();
+
+
+    // 印出結果
+    System.out.printf("(AMERICAN LEAGUE)\n");
+    System.out.printf("%-5s %d -----\n", al_six, 6);
+    System.out.printf("%-5s %d ----- ? -----\n", al_three, 3);
+    System.out.printf("        %-5s %d ----- ?\n", al_two, 2);
+    System.out.printf("%-5s %d -----\n", al_five, 5);
+    System.out.printf("%-5s %d ----- ? -----\n", al_four, 4);
+    System.out.printf("        %-5s %d ----- ? ----- ?\n", al_one, 1);
+    System.out.printf("                               ---- ?\n");
+
+    System.out.printf("(NATIONAL LEAGUE)\n");
+    System.out.printf("%-5s %d ----- ? ----- ? ----- ?\n", nl_six, 6);
+    System.out.printf("%-5s %d -----\n", nl_three, 3);
+    System.out.printf("        %-5s %d ----\n", nl_two, 2);
+    System.out.printf("%-5s %d ----- ? ----- ?\n", nl_five, 5);
+    System.out.printf("%-5s %d -----\n", nl_four, 4);
+    System.out.printf("        %-5s %d -----\n", nl_one, 1);
+  }
+
+
+  //排序的方法
+  public static ArrayList<Team> sortTeamsByPct(ArrayList<Team> teams) {
+    Collections.sort(teams, new Comparator<Team>() {
+      @Override
+      public int compare(Team o1, Team o2) {
+        // 將 String 型態的 pct 轉換為 double 來進行比較
+        // 由高到低排序
+        int pctCompare = Double.compare(Double.parseDouble(o2.getPct()), Double.parseDouble(o1.getPct()));
+        // -1 表示第一個值小於第二個值
+        // 0 表示兩者相等
+        // 1 表示第一個值大於第二個值
+
+        // 如果 pct 相同，則按隊伍名稱的字母順序排序
+        if(pctCompare == 0) {
+          return o2.getTeamName().compareTo(o1.getTeamName()); // 依照英文字母順序從大到小排序
+        }
+        // 否則按勝率排序
+        return pctCompare;
+      }
+    });
+    return teams; // 返回排序後的球隊列表
+  }
+
+}
---
+++ HW1_MLBSort_0926/src/main/java/org/example/MlbSort.java
@@ -0,0 +1,125 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class MlbSort {
+  // 使用 Map 來將不同區域的球隊分類存放
+  private Map<String, List<Team>> teamsByArea = new HashMap<>();
+
+  // 區域名稱 (final表示這個變數的值是不可改變的，它被初始化後就不能再修改。)
+  private static final String AL_EAST = "AL EAST";
+  private static final String AL_CENTRAL = "AL Central";
+  private static final String AL_WEST = "AL West";
+  private static final String NL_EAST = "NL East";
+  private static final String NL_CENTRAL = "NL Central";
+  private static final String NL_WEST = "NL West";
+
+  // 為每個區域分別創建 List
+  List<Team> alEastTeams = new ArrayList<>();
+  List<Team> alCentralTeams = new ArrayList<>();
+  List<Team> alWestTeams = new ArrayList<>();
+  List<Team> nlEastTeams = new ArrayList<>();
+  List<Team> nlCentralTeams = new ArrayList<>();
+  List<Team> nlWestTeams = new ArrayList<>();
+
+  public void load(String filename) {
+    try(BufferedReader br = new BufferedReader(new FileReader(filename)))  {
+      String line;
+      String currentArea = ""; //當前區域名稱
+      int lineNumber = 0;
+
+      while((line = br.readLine()) != null) {
+        lineNumber++;
+        System.out.println("讀取第 " + lineNumber + " 行: " + line); // 調試輸出
+        // 移除可能的 BOM 標記
+        if (lineNumber == 1 && line.startsWith("\uFEFF")) {
+          line = line.substring(1);
+        }
+
+        // 檢查是否是區域名稱
+        if(line.startsWith(AL_EAST)) {
+          currentArea = AL_EAST;
+          continue;
+        } else if(line.startsWith(AL_CENTRAL)) {
+          currentArea = AL_CENTRAL;
+          continue;
+        } else if(line.startsWith(AL_WEST)) {
+          currentArea = AL_WEST;
+          continue;
+        } else if(line.startsWith(NL_EAST)) {
+          currentArea = NL_EAST;
+          continue;
+        } else if(line.startsWith(NL_CENTRAL)) {
+          currentArea = NL_CENTRAL;
+          continue;
+        } else if(line.startsWith(NL_WEST)) {
+          currentArea = NL_WEST;
+          continue;
+        }
+
+        // 分割球隊資料
+        String[] splitLine = line.split(",");
+        String teamName = splitLine[0];
+        String win = splitLine[1];
+        String lose = splitLine[2];
+        String pctRate = splitLine[3];
+        Team team = new Team(teamName, win, lose, pctRate);
+
+        //判斷當前區域名稱
+        switch(currentArea) {
+          case AL_EAST:
+            alEastTeams.add(team);
+            break;
+          case AL_CENTRAL:
+            alCentralTeams.add(team);
+            break;
+          case AL_WEST:
+            alWestTeams.add(team);
+            break;
+          case NL_EAST:
+            nlEastTeams.add(team);
+            break;
+          case NL_CENTRAL:
+            nlCentralTeams.add(team);
+            break;
+          case NL_WEST:
+            nlWestTeams.add(team);
+            break;
+        }
+      }
+
+      // 將結果存入 Map 中
+      teamsByArea.put(AL_EAST, alEastTeams);
+      teamsByArea.put(AL_CENTRAL, alCentralTeams);
+      teamsByArea.put(AL_WEST, alWestTeams);
+      teamsByArea.put(NL_EAST, nlEastTeams);
+      teamsByArea.put(NL_CENTRAL, nlCentralTeams);
+      teamsByArea.put(NL_WEST, nlWestTeams);
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  //檢查用
+  public void printTeamsByArea() {
+    // 使用 Map.Entry 來遍歷 teamsByArea Map 的每個條目
+    for(Map.Entry<String, List<Team>> entry: teamsByArea.entrySet()) {
+      // 從每個條目中提取區域 (key) 和球隊列表 (value)
+      String area = entry.getKey(); // 區域名稱
+      List<Team> teams = entry.getValue(); // 球隊列表
+
+      // 輸出區域名稱
+      System.out.println("Area: " + area);
+
+      // 使用傳統的 for 迴圈遍歷球隊列表，並輸出每支球隊
+      for (Team team : teams) {
+        System.out.println(team);
+      }
+    }
+  }
+
+}
